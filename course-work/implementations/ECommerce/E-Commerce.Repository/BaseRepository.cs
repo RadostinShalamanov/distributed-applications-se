62097b6b98e54509bbee2e39b4a77cf0a8689ebc
Radostin Shalamanov <radishalamanov@gmail.com>
2026-05-09 00:13:51 +0300

    added Repository pattern

---
+++ course-work/implementations/ECommerce/E-Commerce.Repository/BaseRepository.cs
@@ -0,0 +1,72 @@
+﻿using E_Commerce.Data;
+using E_Commerce.Repository.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Commerce.Repository
+{
+    internal class BaseRepository<T> : IBaseRepository<T> where T : class
+    {
+        private readonly ECommerceDbContext _context;
+        private readonly DbSet<T> _dbSet;
+
+        public BaseRepository(ECommerceDbContext context, DbSet<T> dbSet)
+        {
+            _context = context;
+            _dbSet = dbSet;
+        }
+        public async Task Create(T entity)
+        {
+            await _dbSet.AddAsync(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task Delete(T entity)
+        {
+            await Task.FromResult(_dbSet.Remove(entity));
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task Edit(T entity)
+        {
+            await Task.FromResult(_dbSet.Update(entity));
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<T>> GetAll()
+        {
+            return await GetAll(Array.Empty<Expression<Func<T, object>>>());
+        }
+
+        public async Task<IEnumerable<T>> GetAll(params Expression<Func<T, object>>[] includes)
+        {
+            IQueryable<T> query = _context.Set<T>();
+
+            if (includes != null && includes.Length > 0)
+            {
+                foreach (var include in includes)
+                {
+                    query = query.Include(include);
+                }
+            }
+
+            var result = await query.ToListAsync();
+            return result;
+        }
+
+        public async Task<T> GetById(int id)
+        {
+            return await _dbSet.FindAsync(id);
+        }
+
+        public async Task<T> GetById(object[] id)
+        {
+            return await _dbSet.FindAsync(id);
+        }
+    }
+}
---
+++ course-work/implementations/ECommerce/E-Commerce.Repository/Interfaces/IBaseRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Commerce.Repository.Interfaces
+{
+    public interface IBaseRepository<T> where T : class
+    {
+        public Task<IEnumerable<T>> GetAll();
+        public Task<IEnumerable<T>> GetAll(params Expression<Func<T, object>>[] includes);
+
+        public Task<T> GetById(int id);
+        public Task<T> GetById(object[] id);
+
+        public Task Create(T author);
+
+        public Task Edit(T author);
+
+        public Task Delete(T author);
+    }
+}
