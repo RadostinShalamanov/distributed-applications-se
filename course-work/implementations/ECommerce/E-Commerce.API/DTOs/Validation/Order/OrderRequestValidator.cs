de200fefee6ec84025b37a330cf36fb0512d434f
Radostin Shalamanov <radishalamanov@gmail.com>
2026-05-18 17:56:48 +0300

    added fluent validation

---
+++ course-work/implementations/ECommerce/E-Commerce.API/DTOs/Validation/Order/OrderRequestValidator.cs
@@ -0,0 +1,13 @@
+﻿using E_Commerce.API.DTOs.Order;
+using FluentValidation;
+
+namespace E_Commerce.API.DTOs.Validation.Order
+{
+    public class OrderRequestValidator : AbstractValidator<OrderRequestDto>
+    {
+        public OrderRequestValidator()
+        {
+            RuleFor(x => x.Address).NotEmpty().MaximumLength(80).WithMessage("Address must be present");
+        }
+    }
+}
